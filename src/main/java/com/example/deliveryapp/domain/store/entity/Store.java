1dc05d3e8a2290a3fcde38ed5969b1612e2db0d8
mmihye <smilehye910@naver.com>
2024-09-22 15:52:22 +0900

    [#5] Fix: 가게 메뉴 삭제

--- src/main/java/com/example/deliveryapp/domain/store/entity/Store.java
+++ src/main/java/com/example/deliveryapp/domain/store/entity/Store.java
@@ -1,5 +1,6 @@
 package com.example.deliveryapp.domain.store.entity;
 
+import com.example.deliveryapp.domain.menu.entity.Menu;
 import com.example.deliveryapp.domain.store.enumerate.StoreCategory;
 
 import jakarta.persistence.*;
@@ -20,31 +21,23 @@ public class Store {
 	private Float grade;
 
 	private Long deliveryTip;
-	private String menuName;
-	private Long menuPrice;
 
 	@Enumerated(EnumType.STRING)
 	private StoreCategory category;
 
 	@Builder
-	public Store(String storeName, Long deliveryTip, String menuName, Long menuPrice, StoreCategory category) {
+	public Store(String storeName, Long deliveryTip, StoreCategory category) {
 		this.storeName = storeName;
 		this.deliveryTip = deliveryTip;
-		this.menuName = menuName;
-		this.menuPrice = menuPrice;
 		this.category = category;
 		this.grade = 0F;
 	}
 
-	public void updateData(String storeName, Long deliveryTip, Long menuPrice, String menuName, StoreCategory category) {
+	public void updateData(String storeName, Long deliveryTip, StoreCategory category) {
 		if (storeName != null)
 			this.storeName = storeName;
 		if (deliveryTip != null)
 			this.deliveryTip = deliveryTip;
-		if (menuName != null)
-			this.menuName = menuName;
-		if (menuPrice != null)
-			this.menuPrice = menuPrice;
 		if (category != null)
 			this.category = category;
 	}
