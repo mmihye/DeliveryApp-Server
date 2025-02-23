79dc6ced461f23f7cb73d862dc26df963a2dfa43
mmihye <smilehye910@naver.com>
2025-02-23 12:34:16 +0900

    [#13] 파라미터명 수정

--- src/main/java/com/example/deliveryapp/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/deliveryapp/domain/menu/repository/MenuRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.deliveryapp.domain.menu.entity.Menu;
 
-public interface MenuRepository extends JpaRepository<Menu,Long> {
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 
-	List<Menu> findByIdIn(List<Long> menuList);
+	List<Menu> findByIdIn(List<Long> menuIds);
 }
